c1d9f9c9aa8ef0e19f1ead4cc105956633e5a2b7
SOORYA ASOKAN <75926722+soorya474@users.noreply.github.com>
2025-06-13 14:48:49 +0100

    Update Login.spec.ts

--- tests/Login.spec.ts
+++ tests/Login.spec.ts
@@ -21,46 +21,4 @@ test('Login test',async()=>{
 
 
    
-    // await page.goto("https://www.redbus.in/");
-    // await page.locator('//span[text()="Account"]').click();
-    // await page.locator('#user_sign_in_sign_up').click();
-    // const iframe=await page.frameLocator('iframe[class="modalIframe"]');
-    // await iframe.locator('#mobileNoInp').fill("9999999")
-
-
-    // await page.goto("https://www.saucedemo.com");
     
-    // console.log("Assertion passed");
-    // await page.locator('#user-name').fill("standard_user");
-    // await page.locator('#password').fill("secret_sauce");
-    // await page.locator('#login-button').click();
-    // //wherever text is available use the below//if class name use .classname//
-    // const prod:Locator=await page.locator('text=Products');
-    // await prod.click();
-    // //if css selector 'css=tagname[]'
-    // const menu:Locator=await page.locator('xpath=//button[text()="Open Menu"]');
-    // await menu.click();
-    //if data-testid is given use page.getByTestId('give the vlue of data-testid attribute')
-//in playwright config.ts v can update if data-testid atribute is customized
-
-//await expect(page.getByRole('menubar',{name:'Swag Labs'})).toBeVisible();
-    // const countryDropDwn=('give selector');
-    // await page.selectOption(countryDropDwn,{value:'AD'});
-    // await page.selectOption(countryDropDwn,{label:"Australia"})
-    // await page.selectOption(countryDropDwn,{index:10});
-    // const alloptions=await page.$$(countryDropDwn +'> option');
-    // //$$>>give all the elements $>gives single element
-    // for(const e of  alloptions){
-    //     const text=await e.textContent();
-    //     console.log(text);
-        
-    // }
-    //to give timeout
-    //page.setDefaultTimeout(15000);
-
-    ///Drag and drop
-    // await page.locator('sorce selector').dragTo(page.locator('destination selector'));
-    // //to enter character sequentially with a delay
-    // await page.locator('selectr').pressSequentially('soorya',{timeout:1000});
-   
-//});
